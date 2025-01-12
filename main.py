d4a81a1f0b7732a1038cecb881cd35f283182372
David-Fr-afk <dvdshabo@gmail.com>
2025-01-12 14:17:43 -0500

    using MACD instead of moving avg

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-import yfinance as yahooFinance
+import yfinance as yf
+import pandas as pd
 
-GetFacebookInformation = yahooFinance.Ticker("META")
+data = yf.download('AAPL', start='2015-01-01', end='2025-01-09')
 
-print(GetFacebookInformation.info)
+data_close = data[['Close']]
+
+data['EMA_12'] = data['Close'].ewm(span=12, adjust=False).mean()
+data['EMA_26'] = data['Close'].ewm(span=26, adjust=False).mean()
+
+data['MACD'] = data['EMA_12'] - data['EMA_26']
+data['Signal_Line'] = data['MACD'].ewm(span=9, adjust=False).mean() 
+
+print(data[['Close', 'EMA_12', 'EMA_26', 'MACD', 'Signal_Line']].head())
+
+data_close.dropna(inplace=True)
+
+print(data_close.loc['2015-01-01':'2025-01-07', ['Close', 'SMA_5', 'SMA_10']])
