28cd4d00f8794402db0c61a18aebf16e120b61e8
David-Fr-afk <dvdshabo@gmail.com>
2025-01-17 16:40:32 -0500

    Combined the RSI and MACD to see if stock should be bought,sold, or held.

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 import yfinance as yf
 import pandas as pd
 
-data = yf.download('AAPL', interval='1d', start='2023-01-01', end='2023-12-31')
+
+def calculate_rsi(data, column='Close', window=14):
+    delta = data[column].diff()
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+
+    avg_gain = gain.rolling(window=window, min_periods=1).mean()
+    avg_loss = loss.rolling(window=window, min_periods=1).mean()
+
+    rs = avg_gain / avg_loss
+
+    rsi = 100 - (100 / (1 + rs))
+
+    return rsi
+
+
+data = yf.download('META', interval='1d', start='2022-01-01', end='2022-12-31')
 
 data_close = data[['Close']]
 
 data['EMA_12'] = data['Close'].ewm(span=12, adjust=False).mean()
 data['EMA_26'] = data['Close'].ewm(span=26, adjust=False).mean()
-
 data['MACD'] = data['EMA_12'] - data['EMA_26']
-data['Signal_Line'] = data['MACD'].ewm(span=9, adjust=False).mean() 
+data['Signal_Line'] = data['MACD'].ewm(span=9, adjust=False).mean()
+data['RSI'] = calculate_rsi(data, column='Close', window=14) 
 
-print(data[['Close', 'EMA_12', 'EMA_26', 'MACD', 'Signal_Line']].head())
+data['Buy_Signal'] = (data['MACD'] > data['Signal_Line']) & (data['RSI'] < 30)
 
+data['Sell_Signal'] = (data['MACD'] < data['Signal_Line']) & (data['RSI'] > 70)
+
+data['Action'] = data.apply(
+    lambda row: 'Buy' if row['Buy_Signal'] 
+                else 'Sell' if row['Sell_Signal'] 
+                else 'Hold',
+    axis=1
+)
+print(data[['Close', 'MACD', 'Signal_Line', 'RSI', 'Buy_Signal', 'Sell_Signal']].tail())
